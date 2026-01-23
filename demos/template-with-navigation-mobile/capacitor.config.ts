4c72caeae72cc7fd2abcd0ac7e29c8af2cdd5aef
André Laurentino Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2026-01-23 05:33:45 -0300

    Add Android build files and update configs for Capacitor
    Added generated Android build files for Capacitor integration in the mobile template. Updated angular.json to disable analytics and modified capacitor.config.ts to set the correct webDir path. Minor formatting update in package.json.

--- demos/template-with-navigation-mobile/capacitor.config.ts
+++ demos/template-with-navigation-mobile/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pegasus.mobilenav',
   appName: 'PegasusMobile',
-  webDir: 'dist/template-with-navigation-mobile'
+  webDir: 'dist/template-with-navigation-mobile/browser'
 };
 
 export default config;
